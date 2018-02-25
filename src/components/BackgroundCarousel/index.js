48fcacd5866e2b29e762a72d10d6169054eba781
Eli Salter <esal284@aucklanduni.ac.nz>
2018-02-25 15:41:32 +1300

    added albums down to alligator to background carousel

--- src/components/BackgroundCarousel/index.js
+++ src/components/BackgroundCarousel/index.js
@@ -4,6 +4,10 @@ import Slider from 'react-slick';
 
 import sleepWellBeast from '../../images/albums/sleep_well_beast.jpg';
 import troubleWillFindMe from '../../images/albums/trouble_will_find_me.jpg';
+import highViolet from '../../images/albums/high_violet.jpg';
+import boxer from '../../images/albums/boxer.jpg';
+import alligator from '../../images/albums/alligator.jpg';
+import cherryTree from '../../images/albums/cherry_tree.jpg';
 
 import '../../../node_modules/slick-carousel/slick/slick.css';
 import '../../../node_modules/slick-carousel/slick/slick-theme.css';
@@ -16,17 +20,17 @@ export default function BackgroundCarousel() {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
   };
   return (
     <Slider {...settings}>
       <img src={sleepWellBeast} alt="sleep well beast" />
-      <div><img src={troubleWillFindMe} alt="trouble will find me" /></div>
-      <div><h3>3</h3></div>
-      <div><h3>4</h3></div>
-      <div><h3>5</h3></div>
-      <div><h3>6</h3></div>
+      <img src={troubleWillFindMe} alt="trouble will find me" />
+      <img src={highViolet} alt="high violet" />
+      <img src={boxer} alt="boxer" />
+      <img src={alligator} alt="alligator" />
+      <img src={cherryTree} alt="cherry tree" />
     </Slider>
   );
 }
