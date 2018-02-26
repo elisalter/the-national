068534de85db4f3906aa3bf58908a4511a7876d3
Eli Salter <esal284@aucklanduni.ac.nz>
2018-02-26 22:21:46 +1300

    Feature/initial design (#4)
    * basic changes
    
    * installed reack-slick, css loaders, but can't config weback because haven't ejected from RCA yet.., made images folder to store some art assets and implemented basic carousel
    
    * added albums down to alligator to background carousel
    
    * all albums in background carousel now

--- src/components/BackgroundCarousel/index.js
+++ src/components/BackgroundCarousel/index.js
@@ -4,6 +4,13 @@ import Slider from 'react-slick';
 
 import sleepWellBeast from '../../images/albums/sleep_well_beast.jpg';
 import troubleWillFindMe from '../../images/albums/trouble_will_find_me.jpg';
+import highViolet from '../../images/albums/high_violet.jpg';
+import boxer from '../../images/albums/boxer.jpg';
+import alligator from '../../images/albums/alligator.jpg';
+import cherryTree from '../../images/albums/cherry_tree.jpg';
+import sadSongs from '../../images/albums/sad_songs_for_dirty_lovers.jpg';
+import selfTitled from '../../images/albums/self_titled.jpg';
+import virginiaEp from '../../images/albums/the_virginia_ep.jpg';
 
 import '../../../node_modules/slick-carousel/slick/slick.css';
 import '../../../node_modules/slick-carousel/slick/slick-theme.css';
@@ -22,11 +29,14 @@ export default function BackgroundCarousel() {
   return (
     <Slider {...settings}>
       <img src={sleepWellBeast} alt="sleep well beast" />
-      <div><img src={troubleWillFindMe} alt="trouble will find me" /></div>
-      <div><h3>3</h3></div>
-      <div><h3>4</h3></div>
-      <div><h3>5</h3></div>
-      <div><h3>6</h3></div>
+      <img src={troubleWillFindMe} alt="trouble will find me" />
+      <img src={highViolet} alt="high violet" />
+      <img src={virginiaEp} alt="the virginia ep" />
+      <img src={boxer} alt="boxer" />
+      <img src={alligator} alt="alligator" />
+      <img src={cherryTree} alt="cherry tree" />
+      <img src={sadSongs} alt="sad songs for dirty lovers" />
+      <img src={selfTitled} alt="the national" />
     </Slider>
   );
 }
